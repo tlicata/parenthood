97459b9b9ba16008d79d25ddf5dac552f64ac6f8
Tim Licata <tim@timothylicata.com>
2012-04-05 13:01:15 -0700

    add noir as dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject parenthood "1.0.0-SNAPSHOT"
   :description "Partner IAT"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[noir "1.2.2"]
+                 [org.clojure/clojure "1.3.0"]])
