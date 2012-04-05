288af1fdc709e67f7b01534a8968b154e0320640
Tim Licata <tim@timothylicata.com>
2012-04-05 13:16:23 -0700

    hello world noir app

--- src/parenthood/core.clj
+++ src/parenthood/core.clj
@@ -1,1 +1,9 @@
-(ns parenthood.core)
+(ns parenthood.core
+  (:use [noir.core :only [defpage]])
+  (:require [noir.server :as server]))
+
+(defpage "/" [] "Hello World!")
+
+(defn -main []
+  (let [port (Integer/parseInt (System/getenv "PORT"))]
+    (server/start port)))
