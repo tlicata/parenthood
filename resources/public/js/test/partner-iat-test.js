77750d98a289d242028ed7df7aa0b1e237807e67
Tim Licata <tim@timothylicata.com>
2012-04-07 17:42:10 -0700

    try moving to synchronous tests

--- resources/public/js/test/partner-iat-test.js
+++ resources/public/js/test/partner-iat-test.js
@@ -13,37 +13,21 @@
     };
     var getInstructions = function () {
         return getTable().find("#instructions").html();
-    }
+    };
 
-    var steps = [{
-        desc: "Make sure first block is shown automatically",
-        func: function () {
-            var table = getTable();
-            var instr = getInstructions();
-            ok(table.length == 1 &&
-               table.find("tr").length == 4 &&
-               instr == getBlock().instructions &&
-               instr == getBlock(0).instructions);
-        }
-    }, {
-        desc: "Fake a space button press to advance passed the first screen.",
-        func: function () {
-            fakeSpacePress();
-            var instr = getInstructions();
-            ok(instr == getBlock().instructions &&
-               instr == getBlock(1).instructions);
-        }
-    }];
+    test("Make sure first block is shown automatically", function () {
+        var table = getTable();
+        var instr = getInstructions();
+        ok(table.length == 1 &&
+           table.find("tr").length == 4 &&
+           instr == getBlock().instructions &&
+           instr == getBlock(0).instructions);
+    });
 
-    for (var i = 0, n = steps.length; i < n; i++) {
-         (function (step) {
-             asyncTest(step.desc, function () {
-                 expect(1);
-                 setTimeout(function () {
-                     step.func();
-                     start();
-                 }, 2000);
-             });
-         }(steps[i]));
-    }
+    test("Fake a space button press to advance passed the first screen.", function () {
+        fakeSpacePress();
+        var instr = getInstructions();
+        ok(instr == getBlock().instructions &&
+           instr == getBlock(1).instructions);
+    });
 }());
