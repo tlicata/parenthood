829e161db6cbd7524f1c9d8052330c1b6ccbc213
Tim Licata <tim@timothylicata.com>
2012-04-12 21:08:40 -0700

    use _.shuffle instead of extending jquery with $.shuffle

--- resources/public/js/partner-iat.js
+++ resources/public/js/partner-iat.js
@@ -167,17 +167,6 @@ window.parenthood = (function ($) {
         isLastItem: function (item, array) {
             var index = $.inArray(item, array);
             return index == (array.length - 1);
-        },
-        // Randomize order of array in place.
-        shuffle: function (array) {
-            var tmp, current, top = array.length;
-            if (top) while(--top) {
-                current = Math.floor(Math.random() * (top + 1));
-                tmp = array[current];
-                array[current] = array[top];
-                array[top] = tmp;
-            }
-            return array;
         }
     });
 
@@ -210,7 +199,7 @@ window.parenthood = (function ($) {
         return function () {
             if (!block || isLastTrialInBlock(block, trial)) {
                 block = getNextBlock(block);
-                $.shuffle(block.trials);
+                block.trials = _.shuffle(block.trials);
                 trial = null;
             } else {
                 trial = getNextTrial(block, trial);
