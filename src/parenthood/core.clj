b1196e4d2a2b18bc2c14266062b3ecb8508ec735
Tim Licata <tim@timothylicata.com>
2012-05-09 21:33:25 -0700

    add page to view the study name

--- src/parenthood/core.clj
+++ src/parenthood/core.clj
@@ -1,8 +1,16 @@
 (ns parenthood.core
-  (:use [noir.core :only [defpage]])
-  (:require [noir.server :as server]))
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]
+        [hiccup.page-helpers :only [html5]])
+  (:require [noir.server :as server]
+            [parenthood.db :as db]))
 
 (defpage "/" [] "Hello World!")
+
+(defpage "/study/" []
+  (let [id (first (db/all-studies))
+        study (db/get-study id)]
+    (html (html5 [:p (:name study)]))))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
