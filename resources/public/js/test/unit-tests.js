b50d1d411eae4011ea63746718f6dbe8e223c19c
Tim Licata <tim@timothylicata.com>
2012-05-12 14:43:43 -0700

    some tabs got in here

--- resources/public/js/test/unit-tests.js
+++ resources/public/js/test/unit-tests.js
@@ -13,23 +13,23 @@
         notDeepEqual(obj, objCopy);
     });
 
-	test("Expand counts in trials", function () {
-		var trials = [{word: "test", category: "easter"}, // stays the same
-					  {word: "wonk", category: "awesome", count: -1}, // removed
-					  {word: "work", category: "egg", count: 0}, // removed
-					  {word: "unit", category: "hunt", count: 1}, // stay the same
-					  {word: "yes", category: "please", count: "5"}]; // 5 copies
-		var expanded = parenthood.expand(trials);
+    test("Expand counts in trials", function () {
+        var trials = [{word: "test", category: "easter"}, // stays the same
+                      {word: "wonk", category: "awesome", count: -1}, // removed
+                      {word: "work", category: "egg", count: 0}, // removed
+                      {word: "unit", category: "hunt", count: 1}, // stay the same
+                      {word: "yes", category: "please", count: "5"}]; // 5 copies
+        var expanded = parenthood.expand(trials);
 
-		equal(5, trials.length); // original array is not modified
-		equal(7, expanded.length); // expanded array has proper length
-		equal(3, _.uniq(expanded, false, function (trial, index, array) {
-			// make sure expanded contains only 3 items. kept the
-			// trial with no count specifed, and removed the items
-			// with counts of 0 or negative.
-			return trial.word;
-		}).length);
-	});
+        equal(5, trials.length); // original array is not modified
+        equal(7, expanded.length); // expanded array has proper length
+        equal(3, _.uniq(expanded, false, function (trial, index, array) {
+            // make sure expanded contains only 3 items. kept the
+            // trial with no count specifed, and removed the items
+            // with counts of 0 or negative.
+            return trial.word;
+        }).length);
+    });
 
     test("Replace placeholders with input", function () {
         var trials = [{word:"to"},
