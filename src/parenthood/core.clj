fd035c284044b9767cfc6a07750ec21866fcb1e5
Tim Licata <tim@timothylicata.com>
2012-05-15 22:45:09 -0700

    db can store response json in hashmaps by id

--- src/parenthood/core.clj
+++ src/parenthood/core.clj
@@ -2,10 +2,18 @@
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
         [hiccup.page-helpers :only [html5]])
-  (:require [noir.server :as server]
+  (:require [noir.request :as request]
+            [noir.server :as server]
             [parenthood.db :as db]))
 
 (defpage "/" [] "Hello World!")
+
+(defpage "/response/:id" {:keys [id]}
+  (let [request (request/ring-request)
+        user-agent (get-in request [:headers "user-agent"])
+        ip (:remote-addr request)
+        unique (db/add-response id user-agent ip)]
+    (html (html5 [:p ip] [:p user-agent] [:p unique]))))
 
 (defpage "/study/" []
   (let [id (first (db/all-studies))
--- src/parenthood/db.clj
+++ src/parenthood/db.clj
@@ -14,6 +14,33 @@
 (defn inspect-top-level []
   (get-keys nil))
 
+;; responses
+(defn response-key [id] (str "response:" id))
+(defn response-fix [key]
+  (second (string/split key #"response:")))
+(defn add-response [id user-agent ip]
+  (let [unique (str (redis/incr db "responses"))
+        key (response-key id)
+        body {:ip ip :user-agent user-agent}]
+    (do
+      (redis/hset db key unique (json/json-str body))
+      unique)))
+(defn get-response
+  ([id]
+     (get-response id nil))
+  ([id unique]
+     (let [key (response-key id)]
+       (if (nil? unique)
+         (redis/hkeys db key)
+         (json/read-json (redis/hget db key unique))))))
+(defn del-response
+  ([id]
+     (redis/del db [(response-key id)]))
+  ([id unique]
+     (redis/hdel db (response-key id) unique)))
+(defn all-responses []
+  (map response-fix (get-keys (response-key "*"))))
+
 ;; studies
 (def study-prefix "study:")
 (defn study-fix [key]
