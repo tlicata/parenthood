f1970d85fd9f179e6d3d955e1dd2456ec4676adc
Tim Licata <tim@timothylicata.com>
2012-05-19 11:59:04 -0700

    generate partner.html in clojure

--- src/parenthood/core.clj
+++ src/parenthood/core.clj
@@ -1,19 +1,28 @@
 (ns parenthood.core
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
-        [hiccup.page-helpers :only [html5]])
+        [hiccup.page-helpers :only [html5 include-js]])
   (:require [noir.request :as request]
             [noir.server :as server]
             [parenthood.db :as db]))
 
 (defpage "/" [] "Hello World!")
 
-(defpage "/response/:id" {:keys [id]}
+(defpage "/partner-iat.html" {:keys [id]}
   (let [request (request/ring-request)
         user-agent (get-in request [:headers "user-agent"])
         ip (:remote-addr request)
         unique (db/add-response id user-agent ip)]
-    (html (html5 [:p ip] [:p user-agent] [:p unique]))))
+    (html
+     (html5
+      [:head
+       (include-js
+        "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
+        "/js/lib/underscore-min.js"
+        "/js/partner-iat.js")]
+      [:body
+       [:script {:type "text/javascript"}
+        "$(function(){window.parenthood.init()}());"]]))))
 
 (defpage "/study/" []
   (let [id (first (db/all-studies))
