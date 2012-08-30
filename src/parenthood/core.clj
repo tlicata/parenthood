608d5b0118c9e16e60e0e165f7141f176b2b67c7
Tim Licata <tim@timothylicata.com>
2012-08-30 01:02:16 -0700

    bump js version

--- src/parenthood/core.clj
+++ src/parenthood/core.clj
@@ -15,7 +15,7 @@
           "/js/lib/jquery-1.7.2.min.js"
           "/js/lib/json2.js"
           "/js/lib/underscore-min.js"
-          "/js/partner-iat.js?8")
+          "/js/partner-iat.js?9")
    :body (list [:script {:type "text/javascript"}
                 (str "$(function(){window.parenthood.init(" unique ")}());")])})
 
