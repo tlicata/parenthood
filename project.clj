e9c379a1936b84a308d69a1907c9f1ab38747855
Tim Licata <tim@timothylicata.com>
2015-10-25 17:15:27 -0700

    nrepl (or cider) was erroring with old clojure

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
                  [com.draines/postal "1.8.0"]
                  [hiccup "1.0.2"]
                  [lib-noir "0.3.8"]
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/math.numeric-tower "0.0.1"]])
