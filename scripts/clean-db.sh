5d14a2ab3db2a4352d24ac01109a81b80f60362a
Tim Licata <tim@timothylicata.com>
2015-12-23 22:19:26 -0800

    more script to clean up db

--- scripts/clean-db.sh
+++ scripts/clean-db.sh
@@ -34,3 +34,28 @@ redis-cli HDEL "response:*15002*  <https://parenthood.herokuapp.com/partner-iat.
 
 # "13301" also has "13301?".
 # None with the "?" have response data, so ignore it.
+
+redis-cli HDEL "response:601" "694"
+
+redis-cli HDEL "response:15002" "1296"
+redis-cli HDEL "response:15002" "2103"
+
+redis-cli HDEL "response:15801" "1597"
+
+redis-cli HDEL "response:22101" "4163"
+
+redis-cli HDEL "response:11802" "784"
+
+redis-cli HDEL "response:301" "220"
+redis-cli HDEL "response:301" "223"
+redis-cli HDEL "response:302" "221"
+redis-cli HDEL "response:302" "233"
+redis-cli HDEL "response:302" "234"
+
+redis-cli HDEL "response:15302" "1077"
+
+redis-cli HDEL "response:9002" "1117"
+
+redis-cli HDEL "response:12401" "1416"
+
+redis-cli HDEL "response:21901" "2857"
